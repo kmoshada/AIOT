52e1c424202ac3e718d8a13a01a6b63e1bb180ee
kmoshada <kmoshada2@gmail.com>
2026-02-08 15:02:37 +0530

    version 01

--- API/api.py
+++ API/api.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-import contextlib
+import joblib
 
-class Diabetes(BaseModel):
+
+model01 = joblib.load('predictor01.joblib')
+model02 = joblib.load('predictor02.joblib')
+model03 = joblib.load('predictor03.joblib')
+
+
+""" class Diabetes(BaseModel):
     glucose: float
     age: int
     gender: int
 
-
 class Hypertension(BaseModel):
     gender: int
     age: int
+
     currentSmoker: int
     cigsPerDay: float
     BPMeds: int
@@ -26,8 +32,21 @@ class Hypertension(BaseModel):
 class NFLD(BaseModel):
     age: int
     gender: int
-    BMI: float
+    BMI: float """
 
+class Prediction(BaseModel):
+    gender: int
+    age: int
+    currentSmoker: int
+    cigsPerDay: float
+    BPMeds: int
+    diabetes: int
+    totChol: float
+    sysBP: float
+    diaBP: float
+    BMI: float
+    heartRate: float
+    glucose: float
 
 app = FastAPI()
 
@@ -35,8 +54,42 @@ app = FastAPI()
 def read():
     return {"Hello": "World"}
 
+@app.post("/predict")
+def predict(data: Prediction):
 
+     diabetes_features = [[
+         data.glucose,
+         data.age,
+         data.gender
+     ]]
 
+     hypertension_features = [[
+         data.gender,
+         data.age,
+         data.currentSmoker,
+         data.cigsPerDay,
+         data.BPMeds,
+         data.diabetes,
+         data.totChol, data.sysBP, data.diaBP,
+         data.BMI, data.heartRate, data.glucose
+     ]]
+
+     nfld_features = [[
+         data.age,
+         data.gender,
+         data.BMI
+     ]]
+     
+
+     pred01 = model01.predict(diabetes_features)
+     pred02 = model02.predict(hypertension_features)
+     pred03 = model03.predict(nfld_features)
+
+     return {
+         "Diabetes": pred01[0],
+         "Hypertension": pred02[0],
+         "NFLD": pred03[0]
+     }
 
 if __name__ == "__main__":
     import uvicorn
